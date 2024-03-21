eb7633d964abfc27c0260c4db33ea397f1a9e878
Caesar629 <1934407246@qq.com>
2024-03-22 00:38:11 +0800

    update

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -2,7 +2,7 @@ return {
   'akinsho/toggleterm.nvim',
   config = function()
     require('toggleterm').setup {
-      open_mapping = [[<c-\>]],
+      open_mapping = [[<c-t>]],
       direction = 'float',
       float_opts = {
         border = 'curved',
