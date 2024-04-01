e2707bf175ef05e55b568ca3ccd70bea9e3ca872
Caesar629 <1934407246@qq.com>
2024-04-01 22:52:11 +0800

    update

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,7 +6,6 @@ return {
     require('conform').setup {
       formatters_by_ft = {
         lua = { 'stylua' },
-        cpp = { 'clang-format' }
       },
 
       format_on_save = {
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,33 +1,32 @@
 return {
   'neovim/nvim-lspconfig',
   config = function()
-  local lspconfig = require 'lspconfig'
--- Setup language servers.
-  lspconfig.lua_ls.setup {
-      settings={
-    Lua={
-      diagnostics={
-        globals={'vim'}
+    local lspconfig = require 'lspconfig'
+    -- Setup language servers.
+    lspconfig.lua_ls.setup {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim' }
+          }
+        }
       }
     }
-  }
-  }
-  lspconfig.clangd.setup {}
+    lspconfig.clangd.setup {}
+    lspconfig.cmake.setup {}
 
-vim.keymap.set('n', '<LEADER>=', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<LEADER>-', vim.diagnostic.goto_prev)
+    vim.keymap.set('n', '<LEADER>=', vim.diagnostic.goto_next)
+    vim.keymap.set('n', '<LEADER>-', vim.diagnostic.goto_prev)
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-    local opts = { buffer = ev.buf }
-    --
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-  end,
-})
-
-
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+      callback = function(ev)
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+        local opts = { buffer = ev.buf }
+        --
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+      end,
+    })
   end,
 }
