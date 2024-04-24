fc6fad2e986a73483b2eeb7f6e54674f7b964e0f
Caesar629 <1934407246@qq.com>
2024-04-24 23:29:35 +0800

    update

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,8 +8,6 @@ return {
     require("bufferline").setup {
       options = {
         mode = "buffer",
-        -- 显示id
-        number = "ordinal",
         -- 使用 nvim 内置lsp
         diagnostics = "nvim_lsp",
         -- 左侧让出 nvim-tree 的位置
