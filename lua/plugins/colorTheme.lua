0e66a24d4a96906b974ad34ece9feb422c057e11
Caesar629 <1934407246@qq.com>
2024-05-14 22:54:56 +0800

    update

--- lua/plugins/colorTheme.lua
+++ lua/plugins/colorTheme.lua
@@ -1,10 +1,10 @@
 return {
-  "theniceboy/nvim-deus",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd([[colorscheme deus]])
-  end,
+  -- "theniceboy/nvim-deus",
+  -- lazy = false,
+  -- priority = 1000,
+  -- config = function()
+  --   vim.cmd([[colorscheme deus]])
+  -- end,
 
   -- {
   --   "sainnhe/sonokai",
@@ -26,4 +26,14 @@ return {
   --     vim.cmd [[colorscheme solarized-osaka]]
   --   end,
   -- }
+
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      vim.cmd [[colorscheme tokyonight]]
+    end
+  }
 }
