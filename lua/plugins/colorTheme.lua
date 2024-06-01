06c1ab19c56edee9f3fd9d40cfa1b4badff68c61
Caesar629 <1934407246@qq.com>
2024-06-01 23:42:08 +0800

    up

--- lua/plugins/colorTheme.lua
+++ lua/plugins/colorTheme.lua
@@ -7,14 +7,14 @@ return {
 	-- end,
 
 	-- {
-	--   "sainnhe/sonokai",
-	--   priority = 1000,
-	--   config = function()
-	--     vim.g.sonokai_transparent_background = "1"
-	--     vim.g.sonokai_enable_italic = "1"
-	--     vim.g.sonokai_style = "andromeda"
-	--     vim.cmd.colorscheme("sonokai")
-	--   end,
+	-- 	"sainnhe/sonokai",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.g.sonokai_transparent_background = "1"
+	-- 		vim.g.sonokai_enable_italic = "1"
+	-- 		vim.g.sonokai_style = "andromeda"
+	-- 		vim.cmd.colorscheme("sonokai")
+	-- 	end,
 	-- },
 
 	{
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,63 +1,70 @@
 return {
-  "nvim-treesitter/playground",
-  {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    priority = 1000,
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "java",
-          "c",
-          "cpp",
-          "bash",
-          "lua",
-          "vim",
-          "python",
-          "cmake",
-        },
-        highlight = {
-          enable = true,
-          disable = {}, -- list of language that will be disabled
-        },
-        indent = {
-          enable = false
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection    = "<c-n>",
-            node_incremental  = "<c-n>",
-            node_decremental  = "<c-h>",
-            scope_incremental = "<c-l>",
-          },
-        }
-      })
-    end
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    config = function()
-      local tscontext = require('treesitter-context')
-      tscontext.setup {
-        enable = true,
-        max_lines = 0,        -- How many lines the window should span. Values <= 0 mean no limit
-        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-        line_numbers = true,
-        multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
-        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-        mode = 'cursor',      -- Line used to calculate context. Choices: 'cursor', 'topline'
-        -- Separator between context and content. Should be a single character string, like '-'.
-        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-        separator = nil,
-        zindex = 20, -- The Z-index of the context window
-        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-      }
-      vim.keymap.set("n", "[c", function()
-        tscontext.go_to_context()
-      end, { silent = true })
-    end
-  },
+	{ "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"astro",
+				"cmake",
+				"cpp",
+				"css",
+				"fish",
+				"gitignore",
+				"go",
+				"graphql",
+				"http",
+				"java",
+				"php",
+				"rust",
+				"scss",
+				"sql",
+				"svelte",
+				"bash",
+				"lua",
+			},
+
+			-- matchup = {
+			-- 	enable = true,
+			-- },
+
+			-- https://github.com/nvim-treesitter/playground#query-linter
+			query_linter = {
+				enable = true,
+				use_virtual_text = true,
+				lint_events = { "BufWrite", "CursorHold" },
+			},
+
+			playground = {
+				enable = true,
+				disable = {},
+				updatetime = 25,    -- Debounced time for highlighting nodes in the playground from source code
+				persist_queries = true, -- Whether the query persists across vim sessions
+				keybindings = {
+					toggle_query_editor = "o",
+					toggle_hl_groups = "i",
+					toggle_injected_languages = "t",
+					toggle_anonymous_nodes = "a",
+					toggle_language_display = "I",
+					focus_language = "f",
+					unfocus_language = "F",
+					update = "R",
+					goto_node = "<cr>",
+					show_help = "?",
+				},
+			},
+		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+
+			-- MDX
+			vim.filetype.add({
+				extension = {
+					mdx = "mdx",
+				},
+			})
+			vim.treesitter.language.register("markdown", "mdx")
+		end,
+	},
 }
 
