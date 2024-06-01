ad3f074be9759553e0faa83832c352c67f7ed8a0
Caesar629 <1934407246@qq.com>
2024-06-01 22:42:38 +0800

    update

--- lua/plugins/colorTheme.lua
+++ lua/plugins/colorTheme.lua
@@ -1,39 +1,39 @@
 return {
-  -- "theniceboy/nvim-deus",
-  -- lazy = false,
-  -- priority = 1000,
-  -- config = function()
-  --   vim.cmd([[colorscheme deus]])
-  -- end,
+	-- "theniceboy/nvim-deus",
+	-- lazy = false,
+	-- priority = 1000,
+	-- config = function()
+	-- 	vim.cmd([[colorscheme deus]])
+	-- end,
 
-  -- {
-  --   "sainnhe/sonokai",
-  --   priority = 1000,
-  --   config = function()
-  --     vim.g.sonokai_transparent_background = "1"
-  --     vim.g.sonokai_enable_italic = "1"
-  --     vim.g.sonokai_style = "andromeda"
-  --     vim.cmd.colorscheme("sonokai")
-  --   end,
-  -- },
+	-- {
+	--   "sainnhe/sonokai",
+	--   priority = 1000,
+	--   config = function()
+	--     vim.g.sonokai_transparent_background = "1"
+	--     vim.g.sonokai_enable_italic = "1"
+	--     vim.g.sonokai_style = "andromeda"
+	--     vim.cmd.colorscheme("sonokai")
+	--   end,
+	-- },
 
-  -- {
-  --   "craftzdog/solarized-osaka.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {},
-  --   config = function()
-  --     vim.cmd [[colorscheme solarized-osaka]]
-  --   end,
-  -- }
+	{
+		"craftzdog/solarized-osaka.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			vim.cmd [[colorscheme solarized-osaka]]
+		end,
+	}
 
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      vim.cmd [[colorscheme tokyonight]]
-    end
-  }
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	opts = {},
+	-- 	config = function()
+	-- 		vim.cmd [[colorscheme tokyonight]]
+	-- 	end
+	-- }
 }
